bb2a4e113aa522d62013470d4647d660239d01a1
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-01-24 15:03:02 -0500

    Add model data and auto-observation

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 YUI({
     combine: false,
     filter : 'raw'
-}).use('app-base', function (Y) {
+}).use('app-base', 'model', function (Y) {
     var ReactView = Y.Base.create('reactView', Y.View, [], {
+        initializer: function () {
+            // Auto-observe Models and ModelLists and render when they change.
+            Y.Object.each(this.getAttrs(), function (val) {
+                if (val && (val._isYUIModel || val._isYUIModelList)) {
+                    val.after('change', this.render, this);
+                }
+            }, this);
+        },
+
         render: function () {
             var component = this.get('component')(this.toJSON());
             React.renderComponent(component, this.get('container').getDOMNode());
@@ -28,6 +37,7 @@ YUI({
         render: function () {
             return React.DOM.div(null, [
                 React.DOM.h2(null, 'Home!'),
+                React.DOM.p(null, 'Time: ' + this.props.time.now),
                 React.DOM.a({href: '/about/'}, 'about')
             ]);
         }
@@ -48,23 +58,31 @@ YUI({
 
         views: {
             home: {
-                instance: new ReactView({component: Home}),
+                type    : ReactView,
                 preserve: true
             },
 
             about: {
-                instance: new ReactView({component: About}),
+                type    : ReactView,
                 preserve: true
             }
         }
     });
 
+    app.time = new Y.Model({now: Date.now()});
+    setInterval(function () {
+        app.time.set('now', Date.now());
+    }, 1000);
+
     app.route('/', function () {
-        this.showView('home', {}, {render: true});
+        this.showView('home', {
+            component: Home,
+            time     : app.time
+        });
     });
 
     app.route('/about/', function () {
-        this.showView('about', {}, {render: true});
+        this.showView('about', {component: About});
     });
 
     app.render().dispatch();
